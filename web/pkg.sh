8cf4037f9f3f868b16dbe03360a5f1bc574776ee
Michael Sproul <micsproul@gmail.com>
2014-08-23 11:47:03 +1000

    Update pkg.sh

--- web/pkg.sh
+++ web/pkg.sh
@@ -2,4 +2,4 @@
 
 # Package the website files into a handy zip archive.
 
-zip -u -b . arduino-power-site *.js *.html *.css raphael/raphael-min.js raphy-charts/compiled/charts.min.js
+zip -u -b . arduino-power-site *.js *.html *.css raphael/raphael-min.js raphy-charts/compiled/charts.min.js moment/moment.min.js
